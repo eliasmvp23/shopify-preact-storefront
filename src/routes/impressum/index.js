eb78d616a18f441ffe43e6bf022bc5bef11ba129
Elias <eliasmvp23@users.noreply.github.com>
2023-04-06 10:28:42 +0200

    Add files via upload

---
+++ src/routes/impressum/index.js
@@ -0,0 +1,15 @@
+import { h, Component } from 'preact';
+import { route } from "preact-router";
+
+const Impressum = () => {
+	return (
+		<div class="container">
+		<div class="content-area">
+<h2>Impressum</h2>
+<p>Lorem ipsum sit dolor amet.</p>
+		</div>
+		</div>
+	);
+};
+export default Impressum;
+
