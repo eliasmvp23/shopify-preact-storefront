62299286616d94346fb80210b7c452ec74fa81dd
Elias <eliasmvp23@users.noreply.github.com>
2023-04-06 10:30:29 +0200

    Add files via upload

---
+++ src/routes/datenschutz/index.js
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+
+const Datenschutz = () => {
+	return (
+		<div class="container">
+		<div class="content-area">
+<h2>Datenschutz</h2>
+<p>Lorem ipsum Datenschutz</p>
+		</div>
+		</div>
+	);
+};
+export default Datenschutz;
